f6ade3c4157bf0743870bf8b48256f9954bdafb7
Marco Kemmerling <contact@kemmerling.me>
2023-01-24 15:12:31 +0100

    alphazero style puct

--- mcts/tree_policies/exploration_terms/puct_term.py
+++ mcts/tree_policies/exploration_terms/puct_term.py
@@ -1,9 +1,9 @@
 from .uct_term import UCTTerm
-
+import math
 
 class PUCTTerm(UCTTerm):
     def val(self, node):
-        return self.exploration_constant * node.prior_prob * (node.parent.visits / (0 + node.visits))
+        return self.exploration_constant * node.prior_prob * (math.sqrt(node.parent.visits) / (1 + node.visits))
 
     def __str__(self):
         return "PUCT"
