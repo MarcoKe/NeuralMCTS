581436df937e7eaf8e8c7f4cfa2ef4d8932853d4
Marco Kemmerling <contact@kemmerling.me>
2023-01-25 19:04:09 +0100

    replace small negative number with -inf

--- mcts/tree_policies/tree_policy.py
+++ mcts/tree_policies/tree_policy.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 from mcts.node import Node
 from mcts.tree_policies.exploitation_terms.exploitation_term import ExploitationTerm
 from mcts.tree_policies.exploration_terms.exploration_term import ExplorationTerm
@@ -24,14 +25,14 @@ class UCTPolicy(TreePolicy):
         self.exploration_term = exploration_term
 
     def select(self, node: Node, add_dirichlet: bool = False):
-        best_uct, child = -1e7, None
+        best_uct, child = -math.inf, None
 
         dirichlet_noise = None
         if add_dirichlet:
             dirichlet_noise = np.random.dirichlet([self.exploration_term.dirichlet_alpha]*len(node.children))
 
         for i, c in enumerate(node.children):
-            if c.visits > 0 and node.visits > 0:
+            # if c.visits > 0 and node.visits > 0:
                 uct = self.exploitation_term.val(c)
                 if add_dirichlet:
                     uct += self.exploration_term.val(c, dirichlet_noise=dirichlet_noise[i])
@@ -41,6 +42,9 @@ class UCTPolicy(TreePolicy):
                 if uct > best_uct:
                     best_uct = uct
                     child = c
+                if uct == math.inf:
+                    return child
+
         if not child:
             child = random.choice(node.children)
 
