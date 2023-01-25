e68a0992f222a2accde408e93e50ba6edafac384
Marco Kemmerling <contact@kemmerling.me>
2023-01-25 19:02:52 +0100

    refactor

--- mcts/evaluation_policies/neural_rollout_policy.py
+++ mcts/evaluation_policies/neural_rollout_policy.py
@@ -10,13 +10,19 @@ class NeuralRolloutPolicy(EvaluationPolicy):
     def evaluate(self, state):
         done = False
 
+        priors = None
+        first_iteration = True
         while not done:
             legal_actions = self.model.legal_actions(state)
-            _, action_probs = self.agent.evaluate_state(self.model.create_obs(state), legal_actions)
+            _, action_probs = self.agent.evaluate_actions(self.model.create_obs(state), legal_actions)
+            if first_iteration:
+                priors = action_probs
+                first_iteration = False
+
             action = torch.argmax(action_probs)
             state, reward, done = self.model.step(state, action)
 
-        return reward
+        return reward, priors
 
     def __str__(self):
         return "NRoll"
