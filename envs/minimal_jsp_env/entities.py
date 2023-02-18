e7ded0433f2b376a80629497e44fe4951342a6a7
Combrzynski-Nogala,Maciej <maciekcn@gmail.com>
2023-02-18 17:32:59 +0100

    total_entropy, reader, writer, jspsolver

--- envs/minimal_jsp_env/entities.py
+++ envs/minimal_jsp_env/entities.py
@@ -1,7 +1,24 @@
 from collections import namedtuple
 from typing import List
+from envs.minimal_jsp_env.util.jsp_solver import JSPSolver
+
+from scipy.stats import entropy
+from collections import Counter
 
 Operation = namedtuple("Operation", ["job_id", "op_id", "machine_type", "duration"])
+
+def calculate_entropy_from_operations_list(operation_types: List) -> float:
+    counts = Counter(operation_types)
+    probabilities = [count/len(operation_types) for count in counts.values()]
+    entropy_val = entropy(probabilities)
+    return entropy_val
+
+def collect_all_operations(jobs: List[List[Operation]]) -> List[tuple]:
+    all_operations = []
+    for job in jobs:
+        job_operations = [(i.machine_type, i.duration) for i in job]
+        all_operations += job_operations
+    return all_operations
 
 
 class JSPInstance:
@@ -12,5 +29,9 @@ class JSPInstance:
         self.num_ops_per_job = num_ops_per_job #todo infer if not given
         self.max_op_time = max_op_time #todo infer if not given
         self.id = id
-        self.opt_time = opt_time
+        self.opt_time = JSPSolver().solve(self)
+
+        all_operations = collect_all_operations(jobs)
+        total_entropy = calculate_entropy_from_operations_list(all_operations)
+        self.relative_entropy = total_entropy / calculate_entropy_from_operations_list(list(range(len(all_operations))))
 
--- envs/minimal_jsp_env/util/jsp_solver.py
+++ envs/minimal_jsp_env/util/jsp_solver.py
@@ -1,6 +1,11 @@
 from util.solver import Solver
 from util.object_factory import ObjectFactory
 
+import collections
+import numpy as np
+import pandas as pd
+from ortools.sat.python import cp_model
+
 
 class JSPSolverFactory(ObjectFactory):
     def get(self, key, **kwargs):
@@ -8,12 +13,134 @@ class JSPSolverFactory(ObjectFactory):
 
 
 class JSPSolver(Solver):
-    @staticmethod
-    def solve(instance):
+    
+    def solve(self, instance):
         if instance.opt_time:
             return instance.opt_time
         else:
-            return 0 # todo
+            # Getting job format compatible with Google OR input
+            google_or_instance_format = []
+            for job in instance.jobs:
+                job_ops = [[i.machine_type, i.duration] for i in job]
+                google_or_instance_format.append(job_ops)
+
+            instance.opt_time = self._solve_jsp(google_or_instance_format)
+            return instance.opt_time
+
+
+    @staticmethod
+    def _solve_jsp(jobs_data, max_time_in_seconds=7200, verbose=False):
+        """Minimal jobshop problem."""
+        # Create the model.
+        model = cp_model.CpModel()
+
+        machines_count = 1 + max(task[0] for job in jobs_data for task in job)
+        all_machines = range(machines_count)
+
+        # Computes horizon dynamically as the sum of all durations.
+        horizon = sum(task[1] for job in jobs_data for task in job)
+
+        # Named tuple to store information about created variables.
+        task_type = collections.namedtuple('task_type', 'start end interval')
+        # Named tuple to manipulate solution information.
+        assigned_task_type = collections.namedtuple('assigned_task_type',
+                                                    'start job index duration')
+
+        # Creates job intervals and add to the corresponding machine lists.
+        all_tasks = {}
+        machine_to_intervals = collections.defaultdict(list)
+
+        for job_id, job in enumerate(jobs_data):
+            for task_id, task in enumerate(job):
+                machine = task[0]
+                duration = task[1]
+                suffix = '_%i_%i' % (job_id, task_id)
+                start_var = model.NewIntVar(0, horizon, 'start' + suffix)
+                end_var = model.NewIntVar(0, horizon, 'end' + suffix)
+                interval_var = model.NewIntervalVar(start_var, duration, end_var,
+                                                    'interval' + suffix)
+                all_tasks[job_id, task_id] = task_type(
+                    start=start_var, end=end_var, interval=interval_var)
+                machine_to_intervals[machine].append(interval_var)
+
+        # Create and add disjunctive constraints.
+        for machine in all_machines:
+            model.AddNoOverlap(machine_to_intervals[machine])
+
+        # Precedences inside a job.
+        for job_id, job in enumerate(jobs_data):
+            for task_id in range(len(job) - 1):
+                model.Add(all_tasks[job_id, task_id +
+                                    1].start >= all_tasks[job_id, task_id].end)
+
+        # Makespan objective.
+        obj_var = model.NewIntVar(0, horizon, 'makespan')
+        model.AddMaxEquality(obj_var, [
+            all_tasks[job_id, len(job) - 1].end
+            for job_id, job in enumerate(jobs_data)
+        ])
+        model.Minimize(obj_var)
+
+        # Solve model.
+        solver = cp_model.CpSolver()
+        solver.parameters.max_time_in_seconds = max_time_in_seconds
+        status = solver.Solve(model)
+
+        if (status == cp_model.OPTIMAL) or (status == cp_model.FEASIBLE):
+            # Create one list of assigned tasks per machine.
+            assigned_jobs = collections.defaultdict(list)
+            for job_id, job in enumerate(jobs_data):
+                for task_id, task in enumerate(job):
+                    machine = task[0]
+                    assigned_jobs[machine].append(
+                        assigned_task_type(
+                            start=solver.Value(all_tasks[job_id, task_id].start),
+                            job=job_id,
+                            index=task_id,
+                            duration=task[1]))
+
+            # Create per machine output lines.
+            output = ''
+            gantt_data = []
+
+            for machine in all_machines:
+                # Sort by starting time.
+                assigned_jobs[machine].sort()
+                sol_line_tasks = 'Machine ' + str(machine) + ': '
+                sol_line = '           '
+
+                for assigned_task in assigned_jobs[machine]:
+                    name = 'job_%i_%i' % (assigned_task.job, assigned_task.index)
+                    # Add spaces to output to align columns.
+                    sol_line_tasks += '%-10s' % name
+
+                    start = assigned_task.start
+                    duration = assigned_task.duration
+                    sol_tmp = '[%i,%i]' % (start, start + duration)
+                    # Add spaces to output to align columns.
+                    sol_line += '%-10s' % sol_tmp
+
+                    # add Job_num,Mach_num,Job_time,Start_time
+                    gantt_data.append([assigned_task.job, machine, duration, start])
+
+                sol_line += '\n'
+                sol_line_tasks += '\n'
+                output += sol_line_tasks
+                output += sol_line
+
+            gantt_data = pd.DataFrame(data=gantt_data, columns=["Job_num", "Mach_num", "Job_time", "Start_time"])
+
+            # Finally print the solution found.
+            optimal_time = solver.ObjectiveValue()
+            if verbose:
+                print("################# OR Solver Solution #################")
+                print('Optimal Schedule Length: %i' % optimal_time)
+                print(output)
+                print("#######################################################")
+            status_dict = {4: 'OPTIMAL', 2: 'FEASIBLE'}
+            return optimal_time#, output, gantt_data, status_dict[status]
+
+
 
 
 jsp_solvers = JSPSolverFactory()
