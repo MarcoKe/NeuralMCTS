ab173d7efae9578ce20c98f590021e593b076097
Marco Kemmerling <contact@kemmerling.me>
2023-04-04 22:42:30 +0200

    model free agent

--- model_free/train_agent_jsp.py
+++ model_free/train_agent_jsp.py
@@ -9,8 +9,9 @@ from experiment_management.setup_experiment import create_env
 env_config = load_yml((Path('data/config/envs/jsp_minimal_002.yml')))
 env, _ = create_env(env_config)
 
-policy_kwargs = dict(activation_fn=torch.nn.modules.activation.Mish)
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="results/tensorboard/stb3_jsp_tensorboard/", policy_kwargs=policy_kwargs)
+policy_kwargs = dict(activation_fn=torch.nn.modules.activation.Mish, net_arch=dict(pi=[256, 256], vf=[256, 256]))
+model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.00005, tensorboard_log="results/tensorboard/stb3_jsp_tensorboard/", policy_kwargs=policy_kwargs)
+# print(model.policy)
 model.learn(total_timesteps=3_000_000)
 # model.save("results/trained_agents/tsp/model_free/ppo_tsp_15_3e6_ent.zip")
 # model = PPO.load("ppo_tsp_15")
