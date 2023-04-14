dd12835eb04c9a02db76a95135e54dc1e8c38499
Marco Kemmerling <marco.kemmerling@ima-ifu.rwth-aachen.de>
2023-04-14 14:01:35 +0200

    tree construction test

--- tests/mcts/test_node.py
+++ tests/mcts/test_node.py
@@ -1,4 +1,5 @@
 from mcts.node import Node
+from pytest import approx
 
 
 def test_is_leaf():
@@ -27,3 +28,42 @@ def test_is_root():
 
     n.expand([1])
     assert not n.children[0].is_root()
+
+
+def backpropagate(n, value):
+    while n.has_parent():
+        n.update(value)
+        n = n.parent
+    n.update(value)
+
+
+def test_construction():
+    root = Node(None, None)
+    l11 = Node(1, root)
+    l12 = Node(2, root)
+    root.children.append(l11)
+    root.children.append(l12)
+
+    l21 = Node(3, l11)
+    l22 = Node(4, l11)
+    l11.children.append(l21)
+    l11.children.append(l22)
+
+    l31 = Node(5, l22)
+    l32 = Node(6, l22)
+    l22.children.append(l31)
+    l22.children.append(l32)
+
+    assert root.visits == 0
+    assert l31.visits == 0
+
+    backpropagate(l21, 0)
+    backpropagate(l31, 0)
+    backpropagate(l32, 10)
+    backpropagate(l12, 1)
+
+    assert root.max_return == 10
+    assert root.returns == 11
+    assert root.value() == approx(11/4)
+    assert l22.visits == 2
+
