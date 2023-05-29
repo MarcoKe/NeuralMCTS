9fc2d9ab3dac53c6008cea53d6015284b12a6046
Marco Kemmerling <contact@kemmerling.me>
2023-05-29 13:41:52 +0200

    store extra child targets in replay buffer

--- training/replay_buffer.py
+++ training/replay_buffer.py
@@ -11,16 +11,20 @@ class ReplayMemory:
         self.obs_buf = np.zeros((size, *obs_dim), dtype=np.float32)
         self.probs_buf = np.zeros((size, probs_dim), dtype=np.float32)
         self.outcomes_buf = np.zeros(size, dtype=np.float32)
+        self.children_val_buf = np.zeros((size, probs_dim))
+        self.children_obs_buf = np.zeros((size, probs_dim, *obs_dim))
         self.ptr, self.size, self.max_size = 0, 0, size
 
-    def store(self, obs, probs, outcomes):
-        for o, p, z in zip(obs, probs, outcomes):
-            self.store_(o, p, z)
+    def store(self, obs, probs, outcomes, children_vals, children_obs):
+        for o, p, z, cv, co in zip(obs, probs, outcomes, children_vals, children_obs):
+            self.store_(o, p, z, cv, co)
 
-    def store_(self, ob, prob, outcome):
+    def store_(self, ob, prob, outcome, cval, cobs):
         self.obs_buf[self.ptr] = ob
         self.probs_buf[self.ptr] = prob
         self.outcomes_buf[self.ptr] = outcome
+        self.children_obs_buf[self.ptr] = cobs
+        self.children_val_buf[self.ptr] = cval
 
         self.ptr = (self.ptr+1) % self.max_size
         self.size = min(self.size+1, self.max_size)
@@ -30,7 +34,10 @@ class ReplayMemory:
         batch = dict(obs=self.obs_buf[idxs],
                      mcts_probs=self.probs_buf[idxs],
                      outcomes=self.outcomes_buf[idxs],
+                     children_vals=self.children_val_buf[idxs],
+                     children_obs=self.children_obs_buf[idxs]
                      )
+
         return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
 
     def __len__(self):
