56d48ef34504510a2c0112a87b50d531e71dfd91
Biliana Naumova <biliana.naumova@rwth-aachen.de>
2023-09-11 19:40:40 +0200

    refactoring

--- envs/minimal_jsp_env/reward_functions/factory.py
+++ envs/minimal_jsp_env/reward_functions/factory.py
@@ -1,6 +1,6 @@
 from util.object_factory import ObjectFactory
 from envs.minimal_jsp_env.reward_functions.opt_gap import OptimalityGapReward
-from envs.minimal_jsp_env.reward_functions.lower_bounds import LowerBoundsDifferenceReward
+from envs.minimal_jsp_env.reward_functions.lower_bound import LowerBoundDifferenceReward
 
 
 class RewardFactory(ObjectFactory):
@@ -10,4 +10,4 @@ class RewardFactory(ObjectFactory):
 
 reward_functions = RewardFactory()
 reward_functions.register_builder('opt_gap', OptimalityGapReward)
-reward_functions.register_builder('lower_bounds', LowerBoundsDifferenceReward)
+reward_functions.register_builder('lower_bound', LowerBoundDifferenceReward)
--- envs/minimal_jsp_env/reward_functions/lower_bound.py
+++ envs/minimal_jsp_env/reward_functions/lower_bound.py
@@ -1,10 +1,10 @@
 import gym
 
 
-class LowerBoundsDifferenceReward(gym.RewardWrapper):
+class LowerBoundDifferenceReward(gym.RewardWrapper):
     def __init__(self, env):
         self.env = env
-        super(LowerBoundsDifferenceReward, self).__init__(env)
+        super(LowerBoundDifferenceReward, self).__init__(env)
 
     def reward(self, reward):
         makespan = - reward
